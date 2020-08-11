bf665f3b8c1a213cda2fd023fd08589e54c0d697
Chris Calhoun <ctcalhoun2@gmail.com>
2020-08-10 21:14:14 -0500

    add pseudo code for javascript function to highlight contact link

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
-'use strict'
-'console.log("Sort the jort port by cohort. Abort!")
+'use strict';
+// --------------Home page--------------
+// when you click on the contact link
 
+// you want the active class to be removed from the about link
+
+// and the active class to be added to the contact link.
+
+// --------------End Home page--------------
 // *** Fashion Show Page JS***
-
-
 
 // *** End Fashion SHow Page JS ***

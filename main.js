aaff8339ca2d51feed9fa1c3a2819c76ee280c02
Chris Calhoun <ctcalhoun2@gmail.com>
2020-08-11 18:33:19 -0500

    add function to swap active class between contact and about links.

--- main.js
+++ main.js
@@ -1,10 +1,20 @@
 'use strict';
 // --------------Home page--------------
 // when you click on the contact link
+const homeDomEvents = () => {
+  document
+    .querySelector('#contact-link')
+    .addEventListener('click', swapActiveClass);
+};
 
 // you want the active class to be removed from the about link
-
 // and the active class to be added to the contact link.
+const swapActiveClass = e => {
+  const aboutElement = document.querySelector('#about-link');
+  aboutElement.classList.remove('active');
+  e.classList.add('active');
+};
+homeDomEvents();
 
 // --------------End Home page--------------
 // *** Fashion Show Page JS***

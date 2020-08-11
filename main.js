fc4d211eab2c45ed3042d0d2083a165c8f524fd4
Deanna Mix <deanna@LAPTOP-PG68556H.localdomain>
2020-08-10 19:57:57 -0500

    Added a function

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 console.log("Sort the jort port by cohort. Abort!")
 
 // *** Fashion Show Page JS***
-
+const printToDom = (divId, textToPrint) => {
+    const selectedDiv = document.getElementById(divId);
+    selectedDiv.innerHTML = textToPrint;
+  };
 
 
 // *** End Fashion SHow Page JS ***

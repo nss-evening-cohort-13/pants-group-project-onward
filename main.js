6b776f130b979f32b5ca974cd404e115542bfb44
Chris Calhoun <ctcalhoun2@gmail.com>
2020-08-11 20:56:22 -0500

    added seeWindowHref function to look at URL and add active class based on a split string from the URL

--- main.js
+++ main.js
@@ -1,20 +1,42 @@
 'use strict';
 // --------------Home page--------------
 // when you click on the contact link
-const homeDomEvents = () => {
-  document
-    .querySelector('#contact-link')
-    .addEventListener('click', swapActiveClass);
-};
+// const homeDomEvents = () => {};
 
 // you want the active class to be removed from the about link
 // and the active class to be added to the contact link.
-const swapActiveClass = e => {
-  const aboutElement = document.querySelector('#about-link');
-  aboutElement.classList.remove('active');
-  e.classList.add('active');
+
+const seeWindowHref = () => {
+  const testVariable = window.location.href.split('#');
+  if (testVariable[1] === 'targetContact') {
+    document.getElementById('contact-page').classList.add('active');
+  }
+  if (
+    testVariable[1] === 'targetAbout' ||
+    testVariable[0].includes('aboutAndContact')
+  ) {
+    document.getElementById('about-page').classList.add('active');
+  }
 };
-homeDomEvents();
+seeWindowHref();
+
+const changeAboutContactLinkColor = e => {
+  if (e.target.id === 'contact-page') {
+    document.getElementById('about-page').classList.remove('active');
+    document.getElementById('contact-page').classList.add('active');
+  }
+  if (e.target.id === 'about-page') {
+    document.getElementById('contact-page').classList.remove('active');
+    document.getElementById('about-page').classList.add('active');
+  }
+};
+
+document
+  .querySelector('#contact-page')
+  .addEventListener('click', changeAboutContactLinkColor);
+document
+  .querySelector('#about-page')
+  .addEventListener('click', changeAboutContactLinkColor);
 
 // --------------End Home page--------------
 // *** Fashion Show Page JS***
